c295dc738125bb68bbe01afe36ee40a9e7b2bc1e
shingo <sk.shingo.10@gmail.com>
2025-12-18 00:49:12 +0900

    Initial Commit

---
+++ RoutyTests/RoutyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoutyTests.swift
+//  RoutyTests
+//
+//  Created by 垣原親伍 on 2025/12/18.
+//
+
+import Testing
+@testable import Routy
+
+struct RoutyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ RoutyUITests/RoutyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RoutyUITestsLaunchTests.swift
+//  RoutyUITests
+//
+//  Created by 垣原親伍 on 2025/12/18.
+//
+
+import XCTest
+
+final class RoutyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
